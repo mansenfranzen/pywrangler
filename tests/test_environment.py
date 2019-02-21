936d616aa0c4c5a149b812e2ec7f1d16da9cd06b
mansenfranzen <franz.woellert@gmail.com>
2019-02-21 11:13:46 +0100

    Add test for correct java environment if pyspark tests are run. Additionally, test successful pyspark import and pandas-pyspark interaction.

---
+++ tests/test_environment.py
@@ -0,0 +1,57 @@
+"""Check for working test environment.
+
+"""
+
+import os
+import subprocess
+
+import pytest
+
+
+@pytest.mark.pyspark
+def test_java_environment():
+    """Pyspark requires Java to be available. It uses Py4J to start and
+    communicate with the JVM. Py4J looks for JAVA_HOME or falls back calling
+    java directly. This test explicitly checks for the java prerequisites for
+    pyspark to work correctly.
+
+    """
+
+    java_home = os.environ.get("JAVA_HOME")
+
+    java_version = subprocess.run(["java", "-version"],
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT,
+                                  universal_newlines=True)
+
+    if (java_home is None) and (java_version.returncode != 0):
+        raise EnvironmentError("Java setup broken.")
+
+
+@pytest.mark.pyspark
+def test_pyspark_import():
+    """Fail if pyspark can't be imported.
+
+    """
+
+    try:
+        import pyspark
+        print(pyspark.__version__)
+    except (ImportError, ModuleNotFoundError):
+        pytest.fail("pyspark can't be imported")
+
+
+@pytest.mark.pyspark
+def test_pyspark_pandas_interaction(spark_session):
+    """Check simple interaction between pyspark and pandes.
+
+    """
+
+    import pandas as pd
+    import numpy as np
+
+    df_pandas = pd.DataFrame(np.random.rand(10, 2), columns=["a", "b"])
+    df_spark = spark_session.createDataFrame(df_pandas)
+    df_converted = df_spark.toPandas()
+
+    pd.testing.assert_frame_equal(df_pandas, df_converted)
