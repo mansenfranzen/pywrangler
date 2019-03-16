ccc65e1d8773e380a37c792af6e65fd1fa730a00
mansenfranzen <franz.woellert@gmail.com>
2019-03-16 15:07:10 +0100

    Streamline naming convention for `wrangler` and add tests for unused `order_columns` and `groupby_columns` parameters.

--- tests/wranglers/pandas/test_interval_identifier.py
+++ tests/wranglers/pandas/test_interval_identifier.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 from pywrangler.wranglers.pandas.interval_identifier import (
@@ -34,8 +33,6 @@ try:
 except ImportError:
     from pandas.util.testing import assert_frame_equal
 
-
-
 MARKER_TYPES = {"string": {"start": "start",
                            "end": "end",
                            "noise": "noise"},
@@ -56,7 +53,7 @@ MARKERS_KWARGS = dict(argnames='marker',
 
 WRANGLER = (NaiveIterator, VectorizedCumSum)
 WRANGLER_IDS = [x.__name__ for x in WRANGLER]
-WRANGLER_KWARGS = dict(argnames='algorithm',
+WRANGLER_KWARGS = dict(argnames='wrangler',
                        argvalues=WRANGLER,
                        ids=WRANGLER_IDS)
 
@@ -74,25 +71,48 @@ TEST_CASES = (no_interval, single_interval, single_interval_spanning,
               groupby_multiple_intervals_reverse,
               multiple_groupby_order_columns_reverse, start_marker_left_open,
               end_marker_begins)
-TEST_IDS = [x.__name__ for x in TEST_CASES]
-TEST_KWARGS = dict(argnames='test_case',
-                   argvalues=TEST_CASES,
-                   ids=TEST_IDS)
+TEST_CASE_IDS = [x.__name__ for x in TEST_CASES]
+TEST_CASE_KWARGS = dict(argnames='test_case',
+                        argvalues=TEST_CASES,
+                        ids=TEST_CASE_IDS)
+
+TEST_CASES_NO_ORDER_GROUP = (no_interval, single_interval,
+                             single_interval_spanning,
+                             starts_with_single_interval,
+                             ends_with_single_interval,
+                             multiple_intervals, multiple_intervals_spanning,
+                             invalid_end, invalid_start,
+                             start_marker_left_open,
+                             end_marker_begins)
+TEST_CASES_NO_ORDER_GROUP_IDS = [x.__name__ for x in TEST_CASES_NO_ORDER_GROUP]
+TEST_CASES_NO_ORDER_GROUP_KWARGS = dict(argnames='test_case',
+                                        argvalues=TEST_CASES_NO_ORDER_GROUP,
+                                        ids=TEST_CASES_NO_ORDER_GROUP_IDS)
+
+GROUPBY_ORDER_TYPES = {'no_order': {'groupby_columns': 'groupby'},
+                       'no_groupby': {'order_columns': 'order'},
+                       'no_order_no_groupby': {}}
+GROUPBY_ORDER = GROUPBY_ORDER_TYPES.values()
+GROUPBY_ORDER_IDS = list(GROUPBY_ORDER_TYPES.keys())
+GROUPBY_ORDER_KWARGS = dict(argnames='groupby_order',
+                            argvalues=GROUPBY_ORDER,
+                            ids=GROUPBY_ORDER_IDS)
 
 
 @pytest.mark.parametrize(**SHUFFLE_KWARGS)
 @pytest.mark.parametrize(**MARKERS_KWARGS)
-@pytest.mark.parametrize(**TEST_KWARGS)
+@pytest.mark.parametrize(**TEST_CASE_KWARGS)
 @pytest.mark.parametrize(**WRANGLER_KWARGS)
-def test_pandas_interval_identifier(test_case, algorithm, marker, shuffle):
-    """Tests against all available algorithms and test cases.
+def test_groupby_order_columns(test_case, wrangler, marker, shuffle):
+    """Tests against all available wranglers and test cases for different input
+    types and shuffled data.
 
     Parameters
     ----------
     test_case: function
         Generates test data for given test case. Refers to `TEST_CASES`.
-    algorithm: pywrangler.wrangler.interfaces.IntervalIdentifier
-        Refers to the actual wrangler begin tested. See `WRANGLER`.
+    wrangler: pywrangler.wrangler_instance.interfaces.IntervalIdentifier
+        Refers to the actual wrangler_instance begin tested. See `WRANGLER`.
     marker: dict
         Defines the type of data which is used to generate test data. See
         `MARKERS`.
@@ -122,19 +142,57 @@ def test_pandas_interval_identifier(test_case, algorithm, marker, shuffle):
                   "ascending": sort_order}
 
     elif n_cols == 5:
-        kwargs = {"order_columns": ("order1" ,"order2"),
+        kwargs = {"order_columns": ("order1", "order2"),
                   "groupby_columns": ("groupby1", "groupby2"),
-                  "ascending": sort_order*2}
+                  "ascending": sort_order * 2}
 
     else:
         raise ValueError("Incorrect number of columns for test data. "
                          "See module test_data/interval_identifier.py")
 
-    # instantiate actual wrangler
-    wrangler = algorithm(marker_column="marker",
-                         marker_start=marker["start"],
-                         marker_end=marker["end"],
-                         **kwargs)
+    # instantiate actual wrangler_instance
+    wrangler_instance = wrangler(marker_column="marker",
+                                 marker_start=marker["start"],
+                                 marker_end=marker["end"],
+                                 **kwargs)
 
-    test_output = wrangler.fit_transform(test_input)
+    test_output = wrangler_instance.fit_transform(test_input)
+    assert_frame_equal(test_output, expected)
+
+
+@pytest.mark.parametrize(**GROUPBY_ORDER_KWARGS)
+@pytest.mark.parametrize(**TEST_CASES_NO_ORDER_GROUP_KWARGS)
+@pytest.mark.parametrize(**WRANGLER_KWARGS)
+def test_no_groupby_order_columns(test_case, wrangler, groupby_order):
+    """Tests wranglers while not defining order or/and groupby columns on a
+    subset of test cases which do not have a specific order or groupby.
+
+    Parameters
+    ----------
+    test_case: function
+        Generates test data for given test case. Refers to
+        `TEST_CASES_NO_ORDER_GROUP`.
+    wrangler: pywrangler.wrangler_instance.interfaces.IntervalIdentifier
+        Refers to the actual wrangler_instance begin tested. See `WRANGLER`.
+    groupby_order dict
+        Defines the order and groupby columns. See `GROUPBY_ORDER_TYPES`.
+
+    """
+
+    marker = MARKER_TYPES["int"]
+
+    # generate test_input and expected result
+    test_input, expected = test_case(start=marker["start"],
+                                     end=marker["end"],
+                                     noise=marker["noise"],
+                                     target_column_name="iids",
+                                     shuffle=False)
+
+    # instantiate actual wrangler_instance
+    wrangler_instance = wrangler(marker_column="marker",
+                                 marker_start=marker["start"],
+                                 marker_end=marker["end"],
+                                 **groupby_order)
+
+    test_output = wrangler_instance.fit_transform(test_input)
     assert_frame_equal(test_output, expected)
