8e0800b0f1f0eecac0b0d41924e9b1edecac7f9c
mansenfranzen <franz.woellert@gmail.com>
2019-03-16 14:59:23 +0100

    Move commonly used types into separate module.

---
+++ src/pywrangler/util/types.py
@@ -0,0 +1,8 @@
+"""This module contains type definitions.
+
+"""
+
+from typing import Iterable, Union
+
+TYPE_COLUMNS = Union[str, Iterable[str], None]
+TYPE_ASCENDING = Union[bool, Iterable[bool], None]
