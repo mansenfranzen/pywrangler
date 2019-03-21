075070ca3f49644edde3761ae66caaf4f26ad930
mansenfranzen <franz.woellert@gmail.com>
2019-03-21 21:24:05 +0100

    Add `sizeof` pretty printer. Add `align_values` and `align_width` to `enumeration`.

--- src/pywrangler/util/_pprint.py
+++ src/pywrangler/util/_pprint.py
@@ -67,7 +67,8 @@ def header(name: str, indent: int = 0, underline: str = "-") -> str:
     return _join([_header, _underline])
 
 
-def enumeration(values: ENUM, indent: int = 0, bullet_char: str = "-") -> str:
+def enumeration(values: ENUM, indent: int = 0, bullet_char: str = "-",
+                align_values: bool = True, align_width: int = 0) -> str:
     """Create enumeration with bullet points.
 
     Parameters
@@ -78,6 +79,12 @@ def enumeration(values: ENUM, indent: int = 0, bullet_char: str = "-") -> str:
         Indentation count.
     bullet_char: str, optional
         Bullet character.
+    align_values: bool, optional
+        If dict is provided, align all values to the same column. The longest
+        key defines the exact position.
+    align_width: int, optional
+        If dict is provided and `align_values` is True, manually set the align
+        width.
 
     Returns
     -------
@@ -86,7 +93,14 @@ def enumeration(values: ENUM, indent: int = 0, bullet_char: str = "-") -> str:
     """
 
     if isinstance(values, dict):
-        _values = ["{key}: {value}".format(key=key, value=value)
+        fstring = "{key:>{align_width}}: {value}"
+        if align_values and not align_width:
+            align_width = max([len(x) for x in values.keys()])
+
+        _values = [fstring.format(key=key,
+                                  value=value,
+                                  align_width=align_width)
+
                    for key, value in sorted(values.items())]
     else:
         _values = values
@@ -95,3 +109,46 @@ def enumeration(values: ENUM, indent: int = 0, bullet_char: str = "-") -> str:
     indented = _indent(with_bullets, indent)
 
     return _join(indented)
+
+
+def sizeof(size: float, precision: int = 2, align: str = ">",
+           width=None) -> str:
+    """Helper function to format size in human readable format.
+
+    Parameters
+    ----------
+    size: float
+        The size in bytes to be converted into human readable format.
+    precision: int, optional
+        Define shown precision.
+    align: {'<', '^', '>'}, optional
+        Format align specifier.
+    width: int
+        Define maximum width for number.
+
+    Returns
+    -------
+    human_fmt: str
+        Human readable representation of given `size`.
+
+    Notes
+    -----
+    Credit to https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+
+    """  # noqa: E501
+
+    template = "{size:{align}{width}.{precision}f} {unit}B"
+
+    if width is None:
+        width = precision + 5
+
+    kwargs = dict(width=width, precision=precision, align=align)
+
+    # iterate units (multiples of 1024 bytes)
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(size) < 1024.0:
+            return template.format(size=size, unit=unit, **kwargs)
+        size /= 1024.0
+
+    else:
+        return template.format(size=size, unit='Yi', **kwargs)
