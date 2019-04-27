6e8b31157620ac12df5ec27513eda0ad346f42a7
mansenfranzen <franz.woellert@gmail.com>
2019-04-27 14:21:22 +0200

    Refactor tests to use `create_wrangler` fixture. Add sleeps for memory profiling.

--- tests/wranglers/pandas/test_benchmark.py
+++ tests/wranglers/pandas/test_benchmark.py
@@ -4,6 +4,8 @@
 
 import time
 
+import pytest
+
 import numpy as np
 import pandas as pd
 
@@ -17,6 +19,42 @@ from pywrangler.wranglers.pandas.benchmark import (
 MIB = 2 ** 20
 
 
+@pytest.fixture
+def test_wrangler():
+    """Helper fixture to generate PandasWrangler instances with parametrization
+    of transform output and sleep.
+
+    """
+
+    def create_wrangler(size=None, result=None, sleep=0):
+        """Return instance of PandasWrangler.
+
+        Parameters
+        ----------
+        size: float
+            Memory size in MiB to allocate during transform step.
+        result: pd.DataFrame
+            Define extact return value of transform step.
+        sleep: float
+            Define sleep interval.
+
+        """
+
+        class DummyWrangler(PandasSingleNoFit):
+            def transform(self, df):
+                if size is not None:
+                    df_out = pd.DataFrame(allocate_memory(size))
+                else:
+                    df_out = pd.DataFrame(result)
+
+                time.sleep(sleep)
+                return df_out
+
+        return DummyWrangler()
+
+    return create_wrangler
+
+
 def test_pandas_memory_profiler_memory_usage_dfs():
     df1 = pd.DataFrame(np.random.rand(10))
     df2 = pd.DataFrame(np.random.rand(10))
@@ -28,74 +66,54 @@ def test_pandas_memory_profiler_memory_usage_dfs():
     assert PandasMemoryProfiler._memory_usage_dfs(*test_input) == test_output
 
 
-def test_pandas_memory_profiler_return_self():
-    class DummyWrangler(PandasSingleNoFit):
-        def transform(self, df):
-            return pd.DataFrame()
-
-    memory_profiler = PandasMemoryProfiler(DummyWrangler())
+def test_pandas_memory_profiler_return_self(test_wrangler):
+    memory_profiler = PandasMemoryProfiler(test_wrangler())
 
     assert memory_profiler is memory_profiler.profile(pd.DataFrame())
 
 
-def test_pandas_memory_profiler_usage_increases_mean():
-    empty_df = pd.DataFrame()
+def test_pandas_memory_profiler_usage_median(test_wrangler):
+    wrangler = test_wrangler(size=30, sleep=0.01)
+    memory_profiler = PandasMemoryProfiler(wrangler)
 
-    class DummyWrangler(PandasSingleNoFit):
-        def transform(self, df):
-            return pd.DataFrame(allocate_memory(30))
-
-    memory_profiler = PandasMemoryProfiler(DummyWrangler())
-
-    assert memory_profiler.profile(empty_df).median > 29 * MIB
+    assert memory_profiler.profile(pd.DataFrame()).median > 29 * MIB
 
 
-def test_pandas_memory_profiler_usage_input_output():
+def test_pandas_memory_profiler_usage_input_output(test_wrangler):
     df_input = pd.DataFrame(np.random.rand(1000))
     df_output = pd.DataFrame(np.random.rand(10000))
 
     test_df_input = df_input.memory_usage(index=True, deep=True).sum()
     test_df_output = df_output.memory_usage(index=True, deep=True).sum()
 
-    class DummyWrangler(PandasSingleNoFit):
-        def transform(self, df):
-            return df_output
-
-    memory_profiler = PandasMemoryProfiler(DummyWrangler()).profile(df_input)
+    wrangler = test_wrangler(result=df_output)
+    memory_profiler = PandasMemoryProfiler(wrangler).profile(df_input)
 
     assert memory_profiler.input == test_df_input
     assert memory_profiler.output == test_df_output
 
 
-def test_pandas_memory_profiler_usage_ratio():
+def test_pandas_memory_profiler_ratio(test_wrangler):
     usage_mib = 30
     df_input = pd.DataFrame(np.random.rand(1000000))
     usage_input = df_input.memory_usage(index=True, deep=True).sum()
     test_output = ((usage_mib - 1) * MIB) / usage_input
 
-    class DummyWrangler(PandasSingleNoFit):
-        def transform(self, df):
-            return pd.DataFrame(allocate_memory(usage_mib))
+    wrangler = test_wrangler(size=usage_mib, sleep=0.01)
 
-    memory_profiler = PandasMemoryProfiler(DummyWrangler())
+    memory_profiler = PandasMemoryProfiler(wrangler)
 
     assert memory_profiler.profile(df_input).ratio > test_output
 
 
-def test_pandas_time_profiler_fastest():
+def test_pandas_time_profiler_best(test_wrangler):
     """Basic test for pandas time profiler ensuring fastest timing is slower
     than forced sleep.
 
     """
 
     sleep = 0.0001
-    df_input = pd.DataFrame()
-
-    class DummyWrangler(PandasSingleNoFit):
-        def transform(self, df):
-            time.sleep(sleep)
-            return df
-
-    time_profiler = PandasTimeProfiler(DummyWrangler(), 1).profile(df_input)
+    wrangler = test_wrangler(sleep=sleep)
+    time_profiler = PandasTimeProfiler(wrangler, 1).profile(pd.DataFrame())
 
     assert time_profiler.best >= sleep
