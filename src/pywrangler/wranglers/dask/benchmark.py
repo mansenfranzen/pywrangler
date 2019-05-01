b28f6dbf9f14c90f1503419160d878f39c94fc09
mansenfranzen <franz.woellert@gmail.com>
2019-05-01 15:07:18 +0200

    Add `DaskBaseProfiler` and `DaskMemoryProfiler`.

--- src/pywrangler/wranglers/dask/benchmark.py
+++ src/pywrangler/wranglers/dask/benchmark.py
@@ -2,21 +2,88 @@
 
 """
 
-from typing import Union
+import gc
+import sys
+import warnings
+from typing import Callable, List, Union
 
-from dask.dataframe import DataFrame
+import numpy as np
+from dask.diagnostics import ResourceProfiler
 
-from pywrangler.benchmark import TimeProfiler
+from pywrangler.benchmark import MemoryProfiler, TimeProfiler
 from pywrangler.wranglers.dask.base import DaskWrangler
 
 
-class DaskTimeProfiler(TimeProfiler):
+class DaskBaseProfiler:
+    """Define common methods for dask profiler.
+
+    """
+
+    def _wrap_fit_transform(self) -> Callable:
+        """Wrapper function to call `compute()` on dask wrangler instances to
+        enforce computation on lazily evaluated dask graphs.
+
+        Returns
+        -------
+        wrapped: callable
+            Wrapped `fit_transform` method as a function.
+
+        """
+
+        def wrapped(*args, **kwargs):
+            return self.wrangler.fit_transform(*args, **kwargs).compute()
+
+        return wrapped
+
+    @staticmethod
+    def _cache_input(dfs) -> List:
+        """Persist lazily evaluated dask input collections before profiling to
+        capture only relevant `fit_transform`.
+
+        Parameters
+        ----------
+        dfs: iterable
+            Dask collections which can be persisted.
+
+        Returns
+        -------
+        persisted: iterable
+            List of computed dask collections.
+
+        """
+
+        return [df.persist() for df in dfs]
+
+    @staticmethod
+    def _clear_cached_input(dfs):
+        """Remove original reference to previously persisted dask collections
+        to enable garbage collection to free memory. Explicitly check reference
+        count and give warning if persisted dask collections are referenced
+        elsewhere which would prevent memory deallocation.
+
+        Parameters
+        ----------
+        dfs: iterable
+            Persisted dask collections which should be removed.
+
+        """
+
+        # ensure reference counts are updated
+        gc.collect()
+
+        # check ref counts
+        for df in dfs:
+            if sys.getrefcount(df) > 3:
+                warnings.warn("Persisted dask collection is referenced "
+                              "elsewhere and prevents garbage collection",
+                              ResourceWarning)
+
+        dfs.clear()
+
+
+class DaskTimeProfiler(TimeProfiler, DaskBaseProfiler):
     """Approximate time that a dask wrangler instance requires to execute the
     `fit_transform` step.
-
-    Please note, input dataframes are cached before timing execution to ensure
-    timing measurements only capture wrangler's `fit_transform`. This may cause
-    problems if the size of input dataframes exceeds available memory.
 
     Parameters
     ----------
@@ -25,6 +92,10 @@ class DaskTimeProfiler(TimeProfiler):
     repetitions: None, int, optional
         Number of repetitions. If `None`, `timeit.Timer.autorange` will
         determine a sensible default.
+    cache_input: bool, optional
+        Dask collections may be cached before timing execution to ensure
+        timing measurements only capture wrangler's `fit_transform`. By
+        default, it is disabled.
 
     Attributes
     ----------
@@ -54,38 +125,122 @@ class DaskTimeProfiler(TimeProfiler):
     """
 
     def __init__(self, wrangler: DaskWrangler,
-                 repetitions: Union[None, int] = None):
-        self._wrangler = wrangler
+                 repetitions: Union[None, int] = None,
+                 cache_input: bool = False):
+        self.wrangler = wrangler
+        self.cache_input = cache_input
 
-        def wrapper(*args, **kwargs):
-            """Wrapper function to call `compute()` to enforce computation.
+        func = self._wrap_fit_transform()
+        super().__init__(func, repetitions)
 
-            """
-
-            wrangler.fit_transform(*args, **kwargs).compute()
-
-        super().__init__(wrapper, repetitions)
-
-    def profile(self, *dfs: DataFrame, **kwargs):
+    def profile(self, *dfs, **kwargs):
         """Profiles timing given input dataframes `dfs` which are passed to
         `fit_transform`.
 
-        Please note, input dataframes are cached before timing execution to
-        ensure timing measurements only capture wrangler's `fit_transform`.
-        This may cause problems if the size of input dataframes exceeds
-        available memory.
+        """
+
+        if self.cache_input:
+            dfs = self._cache_input(dfs)
+
+        super().profile(*dfs, **kwargs)
+
+        if self.cache_input:
+            self._clear_cached_input(dfs)
+
+        return self
+
+
+class DaskMemoryProfiler(MemoryProfiler, DaskBaseProfiler):
+    """Approximate memory usage that a dask wrangler instance requires to
+    execute the `fit_transform` step.
+
+    Parameters
+    ----------
+    func: callable
+        Callable object to be memory profiled.
+    repetitions: int, optional
+        Number of repetitions.
+    interval: float, optional
+        Defines interval duration between consecutive memory usage
+        measurements in seconds.
+    cache_input: bool, optional
+        Dask collections may be cached before timing execution to ensure
+        timing measurements only capture wrangler's `fit_transform`. By
+        default, it is disabled.
+
+    Attributes
+    ----------
+    measurements: list
+        The actual profiling measurements in bytes.
+    best: float
+        The best measurement in bytes.
+    median: float
+        The median of measurements in bytes.
+    worst: float
+        The worst measurement in bytes.
+    std: float
+        The standard deviation of measurements in bytes.
+    runs: int
+        The number of measurements.
+    baseline_change: float
+        The median change in baseline memory usage across all runs in bytes.
+
+    Methods
+    -------
+    profile
+        Contains the actual profiling implementation.
+    report
+        Print simple report consisting of best, median, worst, standard
+        deviation and the number of measurements.
+    profile_report
+        Calls profile and report in sequence.
+
+    Notes
+    -----
+    The implementation uses dask's own `ResourceProfiler`.
+
+    """
+
+    def __init__(self, wrangler: DaskWrangler,
+                 repetitions: Union[None, int] = 5,
+                 interval: float = 0.01,
+                 cache_input: bool = False):
+        self.wrangler = wrangler
+        self.cache_input = cache_input
+
+        func = self._wrap_fit_transform()
+        super().__init__(func, repetitions, interval)
+
+    def profile(self, *dfs, **kwargs):
+        """Profiles timing given input dataframes `dfs` which are passed to
+        `fit_transform`.
 
         """
 
-        # cache input dataframes
-        dfs_cached = [df.persist() for df in dfs]
+        if self.cache_input:
+            dfs = self._cache_input(dfs)
 
-        super().profile(*dfs_cached, **kwargs)
+        counter = 0
+        baselines = []
+        max_usages = []
 
-        # clear caches
-        for df in dfs_cached:
-            del df
+        while counter < self.repetitions:
+            gc.collect()
 
-        del dfs_cached
+            with ResourceProfiler(dt=self.interval) as rprof:
+                self.func(*dfs, **kwargs)
+
+            mem_usages = [x.mem for x in rprof.results]
+            baselines.append(np.min(mem_usages))
+            max_usages.append(np.max(mem_usages))
+
+            counter += 1
+
+        self._max_usages = max_usages
+        self._baselines = baselines
+        self._measurements = np.subtract(max_usages, baselines).tolist()
+
+        if self.cache_input:
+            self._clear_cached_input(dfs)
 
         return self
--- tests/wranglers/dask/test_benchmark.py
+++ tests/wranglers/dask/test_benchmark.py
@@ -14,30 +14,172 @@ dask = pytest.importorskip("dask")  # noqa: E402
 
 from dask import dataframe as dd
 
-from pywrangler.wranglers.dask.benchmark import DaskTimeProfiler
+from pywrangler.benchmark import allocate_memory
+from pywrangler.wranglers.dask.benchmark import (
+    DaskTimeProfiler,
+    DaskMemoryProfiler,
+    DaskBaseProfiler
+)
 from pywrangler.wranglers.dask.base import DaskSingleNoFit
-
-SLEEP = 0.0001
 
 
 @pytest.fixture
-def wrangler_sleeps():
+def mean_wranger():
     class DummyWrangler(DaskSingleNoFit):
         def transform(self, df):
-            time.sleep(SLEEP)
-            return df
+            return df.mean()
 
-    return DummyWrangler
+    return DummyWrangler()
 
 
-def test_dask_time_profiler_fastest(wrangler_sleeps):
+@pytest.fixture
+def test_wrangler():
+    """Helper fixture to generate DaskWrangler instances with parametrization
+    of transform output and sleep.
+
+    """
+
+    def create_wrangler(size=None, result=None, sleep=0):
+        """Return instance of DaskWrangler.
+
+        Parameters
+        ----------
+        size: float
+            Memory size in MiB to allocate during transform step.
+        result: Dask DataFrame
+            Define extact return value of transform step.
+        sleep: float
+            Define sleep interval.
+
+        """
+
+        class DummyWrangler(DaskSingleNoFit):
+            def transform(self, df):
+                if size is not None:
+                    pdf = pd.DataFrame(allocate_memory(size))
+                    df_out = dd.from_pandas(pdf)
+                elif result is not None:
+                    df_out = result
+                else:
+                    df_out = dd.from_pandas(pd.DataFrame([0]), 1)
+
+                time.sleep(sleep)
+                return df_out
+
+        return DummyWrangler()
+
+    return create_wrangler
+
+
+def test_dask_base_profiler_wrap_fit_transform(test_wrangler):
+    pdf = pd.DataFrame(np.random.rand(50, 5))
+    df = dd.from_pandas(pdf, 5).max().max()
+
+    profiler = DaskTimeProfiler(wrangler=test_wrangler(result=df),
+                                repetitions=1)
+
+    wrapped = profiler._wrap_fit_transform()
+
+    assert callable(wrapped)
+    assert wrapped(df) == pdf.max().max()
+
+
+def test_dask_base_profiler_cache_input():
+    class MockPersist:
+        def persist(self):
+            self.persist_called = True
+            return self
+
+    dask_mocks = [MockPersist(), MockPersist()]
+
+    persisted = DaskBaseProfiler._cache_input(dask_mocks)
+
+    assert all([x.persist_called for x in persisted])
+
+
+def test_dask_base_profiler_clear_cache_input():
+    pdf = pd.DataFrame(np.random.rand(50, 5))
+
+    with pytest.warns(None) as record:
+        DaskBaseProfiler._clear_cached_input([dd.from_pandas(pdf, 5)])
+        assert len(record) == 0
+
+    df = dd.from_pandas(pdf, 5)
+    ref = df  # noqa: F841
+
+    with pytest.warns(ResourceWarning):
+        DaskBaseProfiler._clear_cached_input([df])
+
+
+def test_dask_time_profiler_fastest(test_wrangler):
     """Basic test for dask time profiler ensuring fastest timing is slower
     than forced sleep.
 
     """
 
+    sleep = 0.001
+
     df_input = dd.from_pandas(pd.DataFrame(np.random.rand(10, 10)), 2)
 
-    time_profiler = DaskTimeProfiler(wrangler_sleeps(), 1).profile(df_input)
+    time_profiler = DaskTimeProfiler(wrangler=test_wrangler(sleep=sleep),
+                                     repetitions=1,
+                                     cache_input=True)
 
-    assert time_profiler.best >= SLEEP
+    assert time_profiler.profile(df_input).best >= sleep
+
+
+def test_dask_time_profiler_profile_return_self(test_wrangler):
+    df_input = dd.from_pandas(pd.DataFrame(np.random.rand(10, 10)), 2)
+
+    time_profiler = DaskTimeProfiler(wrangler=test_wrangler(),
+                                     repetitions=1)
+
+    assert time_profiler.profile(df_input) is time_profiler
+
+
+def test_dask_time_profiler_cached_faster(mean_wranger):
+    pdf = pd.DataFrame(np.random.rand(1000000, 10))
+    df_input = dd.from_pandas(pdf, 2).mean()
+
+    time_profiler_no_cache = DaskTimeProfiler(wrangler=mean_wranger,
+                                              repetitions=5,
+                                              cache_input=False)
+
+    time_profiler_cache = DaskTimeProfiler(wrangler=mean_wranger,
+                                           repetitions=5,
+                                           cache_input=True)
+
+    no_cache_time = time_profiler_no_cache.profile(df_input).median
+    cache_time = time_profiler_cache.profile(df_input).median
+
+    assert no_cache_time > cache_time
+
+
+def test_dask_memory_profiler_profile_return_self(test_wrangler):
+    df_input = dd.from_pandas(pd.DataFrame(np.random.rand(10, 10)), 2)
+
+    mem_profiler = DaskMemoryProfiler(wrangler=test_wrangler(),
+                                      repetitions=1)
+
+    assert mem_profiler.profile(df_input) is mem_profiler
+    assert mem_profiler.runs == 1
+
+
+def test_dask_memory_profiler_cached_lower_usage(mean_wranger):
+    pdf = pd.DataFrame(np.random.rand(1000000, 10))
+    df_input = dd.from_pandas(pdf, 5).mean()
+
+    mem_profiler_no_cache = DaskMemoryProfiler(wrangler=mean_wranger,
+                                               repetitions=5,
+                                               cache_input=False,
+                                               interval=0.00001)
+
+    mem_profiler_cache = DaskMemoryProfiler(wrangler=mean_wranger,
+                                            repetitions=5,
+                                            cache_input=True,
+                                            interval=0.00001)
+
+    no_cache_usage = mem_profiler_no_cache.profile(df_input).median
+    cache_usage = mem_profiler_cache.profile(df_input).median
+
+    assert no_cache_usage > cache_usage
