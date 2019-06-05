347cb6a93f1eab03db0ee513d6c55450c790dc3a
mansenfranzen <franz.woellert@gmail.com>
2019-06-05 21:04:30 +0200

    Add SparkBaseProfiler. Add `cache_input` parameter. Add SparkMemoryProfiler template without current implementation.

--- src/pywrangler/wranglers/spark/benchmark.py
+++ src/pywrangler/wranglers/spark/benchmark.py
@@ -2,21 +2,80 @@
 
 """
 
-from typing import Union
+import warnings
+from typing import Callable, Iterable, Union
 
 from pyspark.sql import DataFrame
 
-from pywrangler.benchmark import TimeProfiler
+from pywrangler.benchmark import MemoryProfiler, TimeProfiler
 from pywrangler.wranglers.spark.base import SparkWrangler
 
 
-class SparkTimeProfiler(TimeProfiler):
+class SparkBaseProfiler:
+    """Define common methods for spark profiler.
+
+    """
+
+    def _wrap_fit_transform(self) -> Callable:
+        """Wrapper function to call `count()` on wrangler's `fit_transform`
+        to enforce computation on lazily evaluated spark dataframes.
+
+        Returns
+        -------
+        wrapped: callable
+            Wrapped `fit_transform` method as a function.
+
+        """
+
+        def wrapped(*args, **kwargs):
+            return self.wrangler.fit_transform(*args, **kwargs).count()
+
+        return wrapped
+
+    @staticmethod
+    def _cache_input(dfs: Iterable[DataFrame]):
+        """Persist lazily evaluated spark dataframes before profiling to
+        capture only relevant `fit_transform`. Apply `count` to enforce
+        computation to create cached representation.
+
+        Parameters
+        ----------
+        dfs: iterable
+            Spark dataframes to be persisted.
+
+        Returns
+        -------
+        persisted: iterable
+            List of computed dask collections.
+
+        """
+
+        for df in dfs:
+            df.persist()
+            df.count()
+
+    @staticmethod
+    def _clear_cached_input(dfs: Iterable[DataFrame]):
+        """Unpersist previously persisted spark dataframes after profiling.
+
+        Parameters
+        ----------
+        dfs: iterable
+            Persisted spark dataframes.
+
+        """
+
+        for df in dfs:
+            df.unpersist()
+
+            if df.is_cached:
+                warnings.warn("Spark dataframe could not be unpersisted.",
+                              ResourceWarning)
+
+
+class SparkTimeProfiler(TimeProfiler, SparkBaseProfiler):
     """Approximate time that a spark wrangler instance requires to execute the
     `fit_transform` step.
-
-    Please note, input dataframes are cached before timing execution to ensure
-    timing measurements only capture wrangler's `fit_transform`. This may cause
-    problems if the size of input dataframes exceeds available memory.
 
     Parameters
     ----------
@@ -25,6 +84,10 @@ class SparkTimeProfiler(TimeProfiler):
     repetitions: None, int, optional
         Number of repetitions. If `None`, `timeit.Timer.autorange` will
         determine a sensible default.
+    cache_input: bool, optional
+        Spark dataframes may be cached before timing execution to ensure
+        timing measurements only capture wrangler's `fit_transform`. By
+        default, it is disabled.
 
     Attributes
     ----------
@@ -54,17 +117,13 @@ class SparkTimeProfiler(TimeProfiler):
     """
 
     def __init__(self, wrangler: SparkWrangler,
-                 repetitions: Union[None, int] = None):
-        self._wrangler = wrangler
+                 repetitions: Union[None, int] = None,
+                 cache_input: bool = False):
+        self.wrangler = wrangler
+        self.cache_input = cache_input
 
-        def wrapper(*args, **kwargs):
-            """Wrapper function to call `count()` to enforce computation.
-
-            """
-
-            wrangler.fit_transform(*args, **kwargs).count()
-
-        super().__init__(wrapper, repetitions)
+        func = self._wrap_fit_transform()
+        super().__init__(func, repetitions)
 
     def profile(self, *dfs: DataFrame, **kwargs):
         """Profiles timing given input dataframes `dfs` which are passed to
@@ -77,20 +136,81 @@ class SparkTimeProfiler(TimeProfiler):
 
         """
 
-        # cache input dataframes
-        dfs_cached = [df.cache() for df in dfs]
+        if self.cache_input:
+            self._cache_input(dfs)
 
-        # enforce caching calling count() action
-        for df in dfs_cached:
-            df.count()
+        super().profile(*dfs, **kwargs)
 
-        super().profile(*dfs_cached, **kwargs)
-
-        # clear caches
-        for df in dfs_cached:
-            df.unpersist()
-            del df
-
-        del dfs_cached
+        if self.cache_input:
+            self._clear_cached_input(dfs)
 
         return self
+
+
+class SparkMemoryProfiler(MemoryProfiler, SparkBaseProfiler):
+    """Approximate memory usage that a spark wrangler instance requires to
+    execute the `fit_transform` step.
+
+    #TODO: provide implementation for profile
+
+    Parameters
+    ----------
+    func: callable
+        Callable object to be memory profiled.
+    repetitions: int, optional
+        Number of repetitions.
+    interval: float, optional
+        Defines interval duration between consecutive memory usage
+        measurements in seconds.
+    cache_input: bool, optional
+        Spark dataframes may be cached before timing execution to ensure
+        timing measurements only capture wrangler's `fit_transform`. By
+        default, it is disabled.
+
+    Attributes
+    ----------
+    measurements: list
+        The actual profiling measurements in bytes.
+    best: float
+        The best measurement in bytes.
+    median: float
+        The median of measurements in bytes.
+    worst: float
+        The worst measurement in bytes.
+    std: float
+        The standard deviation of measurements in bytes.
+    runs: int
+        The number of measurements.
+    baseline_change: float
+        The median change in baseline memory usage across all runs in bytes.
+
+    Methods
+    -------
+    profile
+        Contains the actual profiling implementation.
+    report
+        Print simple report consisting of best, median, worst, standard
+        deviation and the number of measurements.
+    profile_report
+        Calls profile and report in sequence.
+
+
+    """
+
+    def __init__(self, wrangler: SparkWrangler,
+                 repetitions: Union[None, int] = 5,
+                 interval: float = 0.01,
+                 cache_input: bool = False):
+        self.wrangler = wrangler
+        self.cache_input = cache_input
+
+        func = self._wrap_fit_transform()
+        super().__init__(func, repetitions, interval)
+
+    def profile(self, *dfs: DataFrame, **kwargs):
+        """Profiles timing given input dataframes `dfs` which are passed to
+        `fit_transform`.
+
+        """
+
+        raise NotImplementedError
