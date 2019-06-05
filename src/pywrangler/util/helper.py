358163d429539bc401e1428845520f9227922c83
mansenfranzen <franz.woellert@gmail.com>
2019-06-05 21:11:00 +0200

    Remove unused `cached_property`.

--- src/pywrangler/util/helper.py
+++ src/pywrangler/util/helper.py
@@ -8,39 +8,6 @@ from typing import Callable, List
 from pywrangler.util.types import T_STR_OPT_MUL
 
 
-def cached_property(method: Callable) -> property:
-    """Decorated method will be called only on first access to calculate a
-    cached property value. After that, the cached value is returned.
-
-    Parameters
-    ---------
-    method: Callable
-        Getter method to be lazily evaluated.
-
-    Returns
-    -------
-    property
-
-    Notes
-    -----
-    Credit goes to python-pptx: https://github.com/scanny/python-pptx/blob/master/pptx/util.py
-
-    """  # noqa: E501
-
-    cache_attr_name = '__{}'.format(method.__name__)
-    docstring = method.__doc__
-
-    def get_prop_value(obj):
-        try:
-            return getattr(obj, cache_attr_name)
-        except AttributeError:
-            value = method(obj)
-            setattr(obj, cache_attr_name, value)
-            return value
-
-    return property(get_prop_value, doc=docstring)
-
-
 def get_param_names(func: Callable,
                     ignore: T_STR_OPT_MUL = None) -> List[str]:
     """Retrieve all parameter names for given function.
