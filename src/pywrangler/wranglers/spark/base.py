f1a89027845b2a0bada5bc1c446c5b9279240056
mansenfranzen <franz.woellert@gmail.com>
2019-06-21 22:10:26 +0200

    Add `validate_columns` and `prepare_orderby` to SparkWrangler.

--- src/pywrangler/wranglers/spark/base.py
+++ src/pywrangler/wranglers/spark/base.py
@@ -2,9 +2,17 @@
 
 """
 
-from pyspark.sql import DataFrame
+from typing import Iterable, Union
 
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+from pyspark.sql.column import Column
+
+from pywrangler.util.sanitizer import ensure_tuple
+from pywrangler.util.types import TYPE_ASCENDING, TYPE_COLUMNS
 from pywrangler.wranglers.base import BaseWrangler
+
+TYPE_OPT_COLUMN = Union[None, Iterable[Column]]
 
 
 class SparkWrangler(BaseWrangler):
@@ -15,6 +23,45 @@ class SparkWrangler(BaseWrangler):
     @property
     def computation_engine(self):
         return "spark"
+
+    @staticmethod
+    def validate_columns(df: DataFrame, columns: TYPE_COLUMNS):
+        """Check that columns exist in dataframe and raise error if otherwise.
+
+        Parameters
+        ----------
+        df: pyspark.sql.DataFrame
+            Dataframe to check against.
+        columns: Tuple[str]
+            Columns to be validated.
+
+        """
+
+        if not columns:
+            return
+
+        columns = ensure_tuple(columns)
+
+        for column in columns:
+            if column not in df.columns:
+                raise ValueError('Column with name `{}` does not exist. '
+                                 'Please check parameter settings.'
+                                 .format(column))
+
+    @staticmethod
+    def prepare_orderby(order_columns: TYPE_COLUMNS,
+                        ascending: TYPE_ASCENDING) -> TYPE_OPT_COLUMN:
+        """Convenient function to return orderby columns in correct
+        ascending/descending order.
+
+        """
+
+        if order_columns is None:
+            return []
+
+        zipped = zip(order_columns, ascending)
+        return [column if ascending else F.desc(column)
+                for column, ascending in zipped]
 
 
 class SparkSingleNoFit(SparkWrangler):
@@ -31,7 +78,7 @@ class SparkSingleNoFit(SparkWrangler):
 
         Parameters
         ----------
-        df: pd.DataFrame
+        df: pyspark.sql.DataFrame
 
         """
 
@@ -46,7 +93,7 @@ class SparkSingleNoFit(SparkWrangler):
 
         Returns
         -------
-        result: pd.DataFrame
+        result: pyspark.sql.DataFrame
 
         """
 
