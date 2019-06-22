4ee77d323f91ace782a27f49ea9705e5dfdd451a
mansenfranzen <franz.woellert@gmail.com>
2019-06-22 14:12:53 +0200

    Test code coverage for new master builds only.

---
+++ tests/travis_coveralls_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Different pandas/pyspark/dask versions are tested separately to avoid
+# irrelevant tests to be run. For example, no spark tests need to be run
+# when pandas wranglers are tested on older pandas versions.
+
+# However, code coverage drops due to many skipped tests. Therefore, there is a
+# master version (marked via env variables) which includes all tests for
+# pandas/pyspark/dask for the newest available versions which is subject to
+# code coverage. Non master versions will not be included in code coverage.
+
+if [[ $ENV_STRING == *"master"* ]]; then
+  coveralls --verbose
+fi
