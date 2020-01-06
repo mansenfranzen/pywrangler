388dd93664cd1a91bbe23937b0c6fe08bf8cb8e1
mansenfranzen <franz.woellert@gmail.com>
2020-01-06 19:25:29 +0100

    Add implementation for FunctionMutant, RandomMutant, ValueMutant and MutantCollection.

--- src/pywrangler/util/testing/mutants.py
+++ src/pywrangler/util/testing/mutants.py
@@ -1,8 +1,12 @@
 """This module contains the data mutants and mutation classes.
 
 """
+import itertools
+from datetime import datetime
 from collections import Counter, defaultdict
-from typing import Any, List, NamedTuple, Sequence
+from typing import Any, List, NamedTuple, Sequence, Callable, Tuple, Iterable
+import random
+from string import ascii_letters
 
 from pywrangler.util.testing.plainframe import PlainFrame
 
@@ -53,7 +57,7 @@ class BaseMutant:
         for mutation in mutations:
             modifications[mutation.column][mutation.row] = mutation.value
 
-        return df.modifiy(modifications)
+        return df.modify(modifications)
 
     @staticmethod
     def _check_duplicated_mutations(mutations: Sequence[Mutation]):
@@ -99,22 +103,128 @@ class BaseMutant:
 
 
 class ValueMutant(BaseMutant):
+    """Represents a Mutant with a single mutation.
+
+    """
 
     def __init__(self, column: str, row: int, value):
         self.column = column
         self.row = row
         self.value = value
 
-    @property
-    def mutations(self) -> List[Mutation]:
+    def generate_mutations(self, df: PlainFrame) -> List[Mutation]:
         mutation = Mutation(column=self.column, row=self.row, value=self.value)
         return [mutation]
 
 
 class FunctionMutant(BaseMutant):
-    pass
+    """Represents a Mutant which wraps a function that essentially generates
+    mutations.
+
+    """
+
+    def __init__(self, func: Callable):
+        self.func = func
+
+    def generate_mutations(self, df: PlainFrame) -> List[Mutation]:
+        return self.func(df)
 
 
 class RandomMutant(BaseMutant):
-    def __init__(self, count=1, columns=None, rows=None):
-        pass
+    """Creates random mutations with naive values for supported dtypes of
+    PlainFrame.
+
+    """
+
+    def __init__(self, count: int = 1, columns: Sequence[str] = None,
+                 rows: Sequence[int] = None, seed: int = 1):
+        self.count = count
+        self.columns = columns
+        self.rows = rows
+        self.seed = seed
+
+    def generate_mutations(self, df: PlainFrame) -> List[Mutation]:
+
+        # set random seed
+        random.seed(self.seed)
+
+        # validate columns and rows
+        columns = self.columns or df.columns
+        rows = self.rows or range(df.n_rows)
+        valid_rows = range(df.n_rows)
+        invalid_rows = set(rows).difference(valid_rows)
+        if invalid_rows:
+            raise ValueError("RandomMutant: Invalid rows provided: {}. "
+                             "Valid rows are: {}"
+                             .format(invalid_rows, valid_rows))
+
+        # validate max count of mutations
+        max_count = len(columns) * len(rows)
+        count = self.count if self.count <= max_count else max_count
+
+        # generate candidates and draw sample
+        candidates = list(itertools.product(columns, rows))
+        sample = random.sample(candidates, count)
+
+        return [self.generate_mutation(df, column, row)
+                for column, row in sample]
+
+    def generate_mutation(self, df: PlainFrame, column: str,
+                          row: int) -> Mutation:
+        """Generates mutation from given PlainFrame and single candidate. A
+        candidate is specified as a tuple of column name and row index.
+
+        """
+
+        plaincolumn = df.get_column(column)
+        value = plaincolumn.values[row]
+        new_value = self._random_value(plaincolumn.dtype, value)
+
+        return Mutation(column=column, row=row, value=new_value)
+
+    def _random_value(self, dtype: str, value: Any) -> Any:
+        """Helper function to generate a random value.
+
+        """
+
+        def _bool():
+            return random.choice([True, False])
+
+        def _int():
+            return random.randint(-10, 10)
+
+        def _float():
+            return random.random()
+
+        def _str():
+            return random.choice(list(ascii_letters))
+
+        def _datetime():
+            year = random.randint(datetime.min.year, datetime.max.year)
+            return datetime(year=year, month=1, day=1)
+
+        func = {"bool": _bool,
+                "int": _int,
+                "float": _float,
+                "str": _str,
+                "datetime": _datetime}[dtype]
+
+        candidate = func()
+        while candidate == value:
+            candidate = func
+
+        return candidate
+
+
+class MutantCollection(BaseMutant):
+    """Represents a collection of multiple Mutant instances.
+
+    """
+
+    def __init__(self, mutants: Iterable):
+        self.mutants = mutants
+
+    def generate_mutations(self, df: PlainFrame) -> List[Mutation]:
+        mutations = [mutant.generate_mutations(df) for mutant in self.mutants]
+
+        return list(itertools.chain.from_iterable(mutations))
