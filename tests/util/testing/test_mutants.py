a33a380380efda07a18251a84b590f639f4acb3f
mansenfranzen <franz.woellert@gmail.com>
2020-01-06 19:23:09 +0100

    Add tests skeleton for mutants.

--- tests/util/testing/test_mutants.py
+++ tests/util/testing/test_mutants.py
@@ -1,3 +1,41 @@
 """This module contains data mutant and mutations tests.
 
 """
+
+
+from pywrangler.util.testing import mutants
+
+
+def test_mutation():
+    # test init
+    # test key
+    pass
+
+def test_base_mutant():
+    # check duplicated mutations
+    # check invalid mutations
+    pass
+
+def test_value_mutant():
+    # test invalid row
+    # test invalid column
+
+    pass
+
+def test_function_mutant():
+    pass
+
+def test_random_mutant():
+    # test invalid row
+    # test invalid column
+    # test max count
+    # test random funcs for all types
+    pass
+
+def test_collection_mutant():
+    # test combination
+    pass
+
+def test_mutant_assertions():
+    # test invalid type changes
+    pass
