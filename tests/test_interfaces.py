20ae6b5f3656b7efb241afd76c56a71811745e67
TobiasRasbold <tobias.rasbold@gmx.de>
2020-01-08 13:22:44 +0100

    streamline concretize abstract interval identifier

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -28,59 +28,58 @@ def concrete_wrangler():
     return concretize_abstract_wrangler(wranglers.IntervalIdentifier)
 
 
-def test_base_interval_identifier_init(ii_kwargs):
+def test_base_interval_identifier_init(ii_kwargs, concrete_wrangler):
 
-    wrangler = concrete_wrangler()
+    wrangler = concrete_wrangler
     bii = wrangler(**ii_kwargs)
 
     assert bii.get_params() == ii_kwargs
 
 
-def test_base_interval_identifier_sort_length_exc(ii_kwargs):
+def test_base_interval_identifier_sort_length_exc(ii_kwargs, concrete_wrangler):
 
     incorrect_length = ii_kwargs.copy()
     incorrect_length["ascending"] = (True, )
 
-    wrangler = concrete_wrangler()
+    wrangler = concrete_wrangler
 
     with pytest.raises(ValueError):
         wrangler(**incorrect_length)
 
 
-def test_base_interval_identifier_sort_keyword_exc(ii_kwargs):
+def test_base_interval_identifier_sort_keyword_exc(ii_kwargs, concrete_wrangler):
 
     incorrect_keyword = ii_kwargs.copy()
     incorrect_keyword["ascending"] = ("wrong keyword", "wrong keyword too")
 
-    wrangler = concrete_wrangler()
+    wrangler = concrete_wrangler
 
     with pytest.raises(ValueError):
         wrangler(**incorrect_keyword)
 
 
-def test_base_interval_identifier_identical_markers(ii_kwargs):
+def test_base_interval_identifier_identical_markers(ii_kwargs, concrete_wrangler):
 
     kwargs = ii_kwargs.copy()
     del kwargs["marker_end"]
 
-    wrangler = concrete_wrangler(kwargs)
+    wrangler = concrete_wrangler(**kwargs)
 
     assert wrangler._identical_start_end_markers is True
 
 
-def test_base_interval_identifier_identical_start_end_markers(ii_kwargs):
+def test_base_interval_identifier_identical_start_end_markers(ii_kwargs, concrete_wrangler):
 
     kwargs = ii_kwargs.copy()
     kwargs["marker_end"] = kwargs["marker_start"]
 
-    wrangler = concrete_wrangler(kwargs)
+    wrangler = concrete_wrangler(**kwargs)
 
     assert wrangler._identical_start_end_markers is True
 
 
-def test_base_interval_identifier_non_identical_markers(ii_kwargs):
+def test_base_interval_identifier_non_identical_markers(ii_kwargs, concrete_wrangler):
 
-    kwargs = ii_kwargs.copy()
-    wrangler = concrete_wrangler(kwargs)
+    wrangler = concrete_wrangler(**ii_kwargs)
 
     assert wrangler._identical_start_end_markers is False
