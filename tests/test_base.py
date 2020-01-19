07e04c854862901f1cb9043586c17d2e9d4249c2
mansenfranzen <franz.woellert@gmail.com>
2020-01-19 22:00:57 +0100

    Add test for correct get_params for extended __init__.

--- tests/test_base.py
+++ tests/test_base.py
@@ -31,7 +31,6 @@ def dummy_wrangler():
 
 
 def test_base_wrangler_not_implemented():
-
     with pytest.raises(TypeError):
         base.BaseWrangler()
 
@@ -50,20 +49,28 @@ def test_base_wrangler_not_implemented():
 
 
 def test_base_wrangler_get_params(dummy_wrangler):
-
     test_output = {"arg1": "arg_val", "kwarg1": "kwarg_val"}
 
     assert dummy_wrangler.get_params() == test_output
 
 
-def test_base_wrangler_properties(dummy_wrangler):
+def test_base_wrangler_get_params_subclassed(dummy_wrangler):
+    class SubClass(dummy_wrangler.__class__):
+        def __init__(self, *args, new_param=2, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.new_param = new_param
 
+    test_output = {"arg1": "arg_val", "kwarg1": "kwarg_val", "new_param": 2}
+
+    assert SubClass("arg_val", "kwarg_val").get_params() == test_output
+
+
+def test_base_wrangler_properties(dummy_wrangler):
     assert dummy_wrangler.preserves_sample_size is True
     assert dummy_wrangler.computation_engine == "DummyEngine"
 
 
 def test_base_wrangler_set_params(dummy_wrangler):
-
     dummy_wrangler.set_params(arg1="new_value")
 
     assert dummy_wrangler.arg1 == "new_value"
@@ -71,6 +78,5 @@ def test_base_wrangler_set_params(dummy_wrangler):
 
 
 def test_base_wrangler_set_params_exception(dummy_wrangler):
-
     with pytest.raises(ValueError):
         dummy_wrangler.set_params(not_exist=0)
