682ae387a380aa00b6e153c3fc2439e51df1bef1
Franz Wöllert <franz.woellert@heidelberg.com>
2020-01-21 15:38:12 +0100

    Add `result_type` parameter and improve doc strings.

--- src/pywrangler/wranglers.py
+++ src/pywrangler/wranglers.py
@@ -25,14 +25,20 @@ class IntervalIdentifier(BaseWrangler):
     values of the first daylight interval are assigned with id 1. All values of
     the second daylight interval will be assigned with id 2 and so on.
 
-    Values which do not belong to any valid interval are assigned the value 0
-    by definition (if start and end marker are identical, there are only
-    invalid values possible before the first start marker is encountered).
+    By default, values which do not belong to any valid interval, are assigned
+    the value 0 by definition (please refer to `result_type` for different
+    result types). If start and end marker are identical or the end marker is
+    not provided, invalid values are only possible before the first start
+    marker is encountered.
 
-    Four Intervals can be selected by using the following params:
+    Due to messy data, start and end marker may occur multiple times in
+    sequence until its counterpart is reached. Therefore, intervals may have
+    different spans based on different task requirements. For example, the very
+    first start or very last start marker may define the correct start of an
+    interval. Accordingly, four intervals can be selected by setting
     `marker_start_use_first` and `marker_end_use_first`. The resulting
-    intervals are:
-        - identical start and end markers
+    intervals are as follows:
+
         - first start / first end
         - first start / last end (longest interval)
         - last start / first end (shortest interval)
@@ -66,6 +72,14 @@ class IntervalIdentifier(BaseWrangler):
         Sort ascending vs. descending. Specify list for multiple sort orders.
         If a list is specified, length of the list must equal length of
         `order_columns`. Default is True.
+    result_type: str, optional
+        Defines the content of the returned result. If 'raw', interval ids
+        will be in arbitrary order with no distinction made between valid and
+        invalid intervals. Intervals are distinguishable by interval id but the
+        interval id may not provide any more information. If 'valid', the
+        result is the same as 'raw' but all invalid intervals are set to 0.
+        If 'enumerated', the result is the same as 'valid' but interval ids
+        increase in ascending order (as defined by order) in steps of one.
     target_column_name: str, optional
         Name of the resulting target column.
 
@@ -80,6 +94,7 @@ class IntervalIdentifier(BaseWrangler):
                  order_columns: TYPE_COLUMNS = None,
                  groupby_columns: TYPE_COLUMNS = None,
                  ascending: TYPE_ASCENDING = None,
+                 result_type: str = "enumerated",
                  target_column_name: str = "iids"):
 
         self.marker_column = marker_column
@@ -90,7 +105,15 @@ class IntervalIdentifier(BaseWrangler):
         self.order_columns = sanitizer.ensure_iterable(order_columns)
         self.groupby_columns = sanitizer.ensure_iterable(groupby_columns)
         self.ascending = sanitizer.ensure_iterable(ascending)
+        self.result_type = result_type
         self.target_column_name = target_column_name
+
+        # check correct result type
+        valid_result_types = {"raw", "valid", "enumerated"}
+        if result_type not in valid_result_types:
+            raise ValueError("Parameter `result_type` is invalid with: {}. "
+                             "Allowed arguments are: {}"
+                             .format(result_type, valid_result_types))
 
         # check for identical start and end values
         self._identical_start_end_markers = ((marker_end == NONEVALUE) or
