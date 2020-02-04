e0e31316937e89183753e02384b71048d31de044
Franz Wöllert <franz.woellert@heidelberg.com>
2020-02-04 13:03:46 +0100

    Add `retain_none` parameter.

--- src/pywrangler/util/sanitizer.py
+++ src/pywrangler/util/sanitizer.py
@@ -4,37 +4,46 @@ conversions.
 """
 
 import collections
-from typing import Any, List, Tuple, Type, Union
+from typing import Any, List, Tuple, Type, Union, Optional
 
 import pandas as pd
 
-ITER_TYPE = Union[List[Any], Tuple[Any], None]
+ITER_TYPE = Optional[Union[List[Any], Tuple[Any]]]
 
 
-def ensure_iterable(values: Any, seq_type: Type = list) -> ITER_TYPE:
+# TODO: Use generic for sequence type
+
+def ensure_iterable(values: Any, seq_type: Type = list,
+                    retain_none: bool = False) -> ITER_TYPE:
     """For convenience, some parameters may accept a single value (string
     for a column name) or multiple values (list of strings for column
     names). Other functions always require a list or tuple of strings. Hence,
     this function ensures that the output is always an iterable of given
-    `constructor` type (list or tuple) while taking care of exceptions just
-    like strings.
+    `constructor` type (list or tuple) while taking care of exceptions like
+    strings.
 
     Parameters
     ----------
     values: Any
         Input values to be converted to tuples.
-    seq_type: {list, tuple}
-        Iterable class to return.
+    seq_type: type
+        Define return container type.
+    retain_none: bool, optional
+        Define behaviour if None is passed. If True, returns None. If False,
+        returns empty
 
     Returns
     -------
-    iterable: List[Any], Tuple[Any]
+    iterable: seq_type
 
     """
 
     # None remains None
     if values is None:
-        return None
+        if retain_none:
+            return None
+        else:
+            return seq_type()
 
     # if not iterable, return iterable with single value
     elif not isinstance(values, collections.Iterable):
--- tests/util/test_sanitizer.py
+++ tests/util/test_sanitizer.py
@@ -7,46 +7,45 @@ import pytest
 from pywrangler.util.sanitizer import ensure_iterable
 
 
-@pytest.mark.parametrize(argnames="type", argvalues=(list, tuple))
-def test_ensure_iterable_number(type):
+@pytest.mark.parametrize(argnames="seq_type", argvalues=(list, tuple))
+def test_ensure_iterable_number(seq_type):
     test_input = 3
-    test_output = type([3])
+    test_output = seq_type([3])
 
-    assert ensure_iterable(test_input, type) == test_output
+    assert ensure_iterable(test_input, seq_type) == test_output
 
 
-@pytest.mark.parametrize(argnames="type", argvalues=(list, tuple))
-def test_ensure_iterable_string(type):
+@pytest.mark.parametrize(argnames="seq_type", argvalues=(list, tuple))
+def test_ensure_iterable_string(seq_type):
     test_input = "test_string"
-    test_output = type(["test_string"])
+    test_output = seq_type(["test_string"])
 
-    assert ensure_iterable(test_input, type) == test_output
+    assert ensure_iterable(test_input, seq_type) == test_output
 
 
-@pytest.mark.parametrize(argnames="type", argvalues=(list, tuple))
-def test_ensure_iterable_strings(type):
+@pytest.mark.parametrize(argnames="seq_type", argvalues=(list, tuple))
+def test_ensure_iterable_strings(seq_type):
     test_input = ["test1", "test2"]
-    test_output = type(["test1", "test2"])
+    test_output = seq_type(["test1", "test2"])
 
-    assert ensure_iterable(test_input, type) == test_output
+    assert ensure_iterable(test_input, seq_type) == test_output
 
 
-@pytest.mark.parametrize(argnames="type", argvalues=(list, tuple))
-def test_ensure_iterable_custom_class(type):
+@pytest.mark.parametrize(argnames="seq_type", argvalues=(list, tuple))
+def test_ensure_iterable_custom_class(seq_type):
     class Dummy:
         pass
 
     dummy = Dummy()
 
     test_input = dummy
-    test_output = type([dummy])
+    test_output = seq_type([dummy])
 
-    assert ensure_iterable(test_input, type) == test_output
+    assert ensure_iterable(test_input, seq_type) == test_output
 
 
-@pytest.mark.parametrize(argnames="type", argvalues=(list, tuple))
-def test_ensure_iterable_none(type):
-    test_input = None
-    test_output = None
+@pytest.mark.parametrize(argnames="seq_type", argvalues=(list, tuple))
+def test_ensure_iterable_none(seq_type):
 
-    assert ensure_iterable(test_input, type) == test_output
+    assert ensure_iterable(None, seq_type) == seq_type()
+    assert ensure_iterable(None, seq_type, retain_none=True) is None
